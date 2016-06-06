6d7c3b47c8e0402db988fb4e58d85ae2d40d1e7f
eduardo <theeduardorfs@gmail.com>
2016-06-06 16:40:22 +0000

    First functional release hahaha

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
+var findup = require('findup-sync');
+var path = require('path');
 
 module.exports = function (options) {
-  options.pattern = options.pattern || ['koa-*', 'koa.*', '@*/koa{-,.}*'];
+  options = options || {};
+  options.pattern = options.pattern || ['koa-*', 'koa.*', '@*/koa{-,.}*', '!koa-load'];
   options.replaceString = options.replaceString || /^koa(-|\.)/;
+  options.config = options.config || findup('package.json', {cwd: parentDir});
   return require('gulp-load-plugins')(options);
 };
+
+var parentDir = path.dirname(module.parent.filename);
